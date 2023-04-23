2dff417b2a17b50f18665f10c84832505c69ef7b
El Duderino <129619767+Duderino-Games@users.noreply.github.com>
2023-04-23 14:03:17 -0700

    Update start-game.bat

--- dev/win32/start-game.bat
+++ dev/win32/start-game.bat
@@ -1,13 +1,12 @@
 @echo off
 
-title Toontown 2 Game Launcher
+title Toontown Online Game Launcher
 
 echo Choose your connection method!
 echo.
 echo #1 - Localhost
 echo #2 - Dev Server
 echo #3 - Custom
-echo #4 - Prod Server
 echo.
 
 :selection
@@ -16,11 +15,9 @@ set INPUT=-1
 set /P INPUT=Selection: 
 
 if %INPUT%==1 (
-    set TTS_GAMESERVER=127.0.0.1
+    set TTS_GAMESERVER=http://localhost:6667
 ) else if %INPUT%==2 (
-    set TTS_GAMESERVER=167.114.220.172
-) else if %INPUT%==4 (
-    SET TTS_GAMESERVER=lw2.ez-webz.com:7198
+    set TTS_GAMESERVER=http://unite.sunrise.games:6667
 ) else if %INPUT%==3 (
     echo.
     set /P TTS_GAMESERVER=Gameserver: 
@@ -33,7 +30,7 @@ echo.
 if %INPUT%==2 (
     set /P ttsUsername="Username: "
     set /P ttsPassword="Password: "
-) else if %INPUT%==4 (
+) else if %INPUT%==2 (
     set /P ttsUsername="Username: "
     set /P ttsPassword="Password: "
 ) else (
@@ -43,12 +40,12 @@ if %INPUT%==2 (
 echo.
 
 echo ===============================
-echo Starting Toontown Online II...
+echo Starting Toontown Online 2: The Second Chapter...
 echo ppython: "C:\Panda3D-1.10.0\python\ppython.exe"
 
 if %INPUT%==2 (
     echo Username: %tt2Username%
-) else if %INPUT%==4 (
+) else if %INPUT%==2 (
     echo Username: %tt2Username%
 ) else (
     echo Username: %TT2_PLAYCOOKIE%
@@ -60,13 +57,7 @@ echo ===============================
 cd ../../
 
 :main
-if %INPUT%==2 (
-    "C:\Panda3D-1.10.0\python\ppython.exe" -m toontown.toonbase.ToontownStartRemoteDB
-) else if %INPUT%==4 (
-    "C:\Panda3D-1.10.0\python\ppython.exe" -m toontown.toonbase.ToontownStartRemoteDB
-) else (
     "C:\Panda3D-1.10.0\python\ppython.exe" -m toontown.toonbase.ToontownStart
-)
 pause
 
 goto main
